7ce66f7ed4a8b3bd9540f77b862df6ef361d9261
Deepak <deepprajapat769@email.com>
2025-02-07 02:32:45 +0530

    Get Topic-Based  Analytics API 01

--- routes/analyticsRoutes.js
+++ routes/analyticsRoutes.js
@@ -1,10 +1,12 @@
 import express from "express";
 import {
   getUrlAnalytics,
+  getTopicAnalytics,
 } from "../controllers/analyticsController.js";
 
 const router = express.Router();
 
 router.get("/analytics/:alias", getUrlAnalytics);
+router.get("/analytics/topic/:topic", getTopicAnalytics); // Topic-based analytics
 
 export default router;
